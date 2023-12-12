3293410a0fa16389157b9bb9c387212e78f873c2
celedev97 <celedev97@gmail.com>
2023-12-12 21:19:20 +0100

    v0.0.1

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,7 @@ game 'rdr3'
 fx_version 'adamant'
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
-author 'VORP @Emolitt | celedev97'
+author 'celedev97'
 
 client_scripts {
   'client/*.lua'
@@ -25,7 +25,7 @@ files {
 }
 
 --dont touch
-version '1.0'
-vorp_checker 'yes'
-vorp_name '^4Resource version Check^3'
-vorp_github 'https://github.com/celedev97/land_of_steam_mailbox'
+version '0.0.1'
+--vorp_checker 'yes'
+--vorp_name '^4Resource version Check^3'
+--vorp_github 'https://github.com/celedev97/land_of_steam_mailbox'
